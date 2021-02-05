03e15ba92ce240f67c6c44d3e2797fbb4e740b3b
Guillaume Raffy <guillaume.raff@univ-rennes1.fr>
2021-02-05 17:26:23 +0100

    it is now possible to create piece definitions and to add them to the material

--- main.c
+++ main.c
@@ -3,29 +3,73 @@
 
 typedef enum {false, true} bool;
 
+// #define DEBUG_MESSAGE(message, ...) (printf(message, __VA_ARGS__);)
+#define DEBUG true
+#define debug_print(fmt, ...) \
+            do { if (DEBUG) fprintf(stderr, fmt, __VA_ARGS__); } while (0)
+
+
+void error(char* pMessage)
+{
+    printf("ERROR: %s\n", pMessage);
+    exit(1);
+}
 
 // array of pointers to structures
 typedef struct
 {
-    int iNum_Elements;
-    void *pElements;
+    int iNumElements;
+    void **ppElements;
 } Array;
 
 void array_init(Array* pArray)
 {
-    pArray->iNum_Elements = 0;
-    pArray->pElements = NULL;
+    pArray->iNumElements = 0;
+    pArray->ppElements = NULL;
 }
 
 void array_empty(Array* pArray)
 {
-    if( pArray->iNum_Elements > 0 )
+    if( pArray->iNumElements > 0 )
     {
-        free(pArray->pElements);
-        pArray->pElements = NULL;
-        pArray->iNum_Elements = 0;
+        free(pArray->ppElements);
+        pArray->ppElements = NULL;
+        pArray->iNumElements = 0;
     }
 }
+
+/*
+    adds an element to the array
+*/
+void array_add(Array* pArray, void* pElement)
+{
+    void** ppOldElements = pArray->ppElements;
+
+    // allocate a bigger array of pointers
+    pArray->ppElements = malloc(sizeof(void*) * (pArray->iNumElements + 1));
+
+    // move the elements from the old elements to the new elements
+    for(int iElement = 0; iElement< pArray->iNumElements; iElement++)
+    {
+        pArray->ppElements[iElement] = ppOldElements[iElement];
+    }
+
+    // now that its contants have been copied, free the old array of pointers
+    if(ppOldElements != NULL)
+    {
+        free(ppOldElements);
+        ppOldElements = NULL;
+    }
+    // free((void *)0x123456);
+
+    // add a pointer to the new element at the end
+    pArray->ppElements[pArray->iNumElements] = pElement;
+
+    pArray->iNumElements++;
+
+    
+}
+
 
 typedef int PieceId;
 
@@ -43,6 +87,70 @@ typedef struct
     int iX;
     int iY;
 } Position;
+
+// maximum number of tiles covered by any piece
+#define MAX_PIECE_SIZE 3
+
+typedef struct 
+{
+    PieceId iPieceId;
+    int iNumTiles;
+    Position aTilesLocations[MAX_PIECE_SIZE];
+    bool bHasHole;
+    Position sHolePos;
+} PieceDef;
+
+PieceDef * piece_def_create(PieceId iPieceId)
+{
+    PieceDef* pPiece = (PieceDef *)malloc(sizeof(PieceDef));
+    pPiece->iPieceId = iPieceId;
+    pPiece->iNumTiles = 0;
+    pPiece->bHasHole = false;
+    return pPiece;
+}
+
+void piece_def_delete(PieceDef* pPiece)
+{
+    debug_print("deleting piece %d\n", pPiece->iPieceId);
+    free(pPiece);
+}
+
+
+void piece_def_print(PieceDef* pPiece)
+{
+    printf("piece %d : ", pPiece->iPieceId);
+    for(int iTileIndex = 0; iTileIndex < pPiece->iNumTiles; iTileIndex++ )
+    {
+        printf(" (%d, %d)", pPiece->aTilesLocations[iTileIndex].iX, pPiece->aTilesLocations[iTileIndex].iY);
+    }
+    if (pPiece->bHasHole)
+    {
+        printf("(hole at %d, %d)", pPiece->sHolePos.iX, pPiece->sHolePos.iY);
+    }
+    printf("\n");
+}
+
+void piece_def_add_tile(PieceDef* pPiece, int iTilePosX, int iTilePosY)
+{
+    if( pPiece->iNumTiles >= MAX_PIECE_SIZE )
+    {
+        error("max number of tiles exceeded");
+    }
+    pPiece->aTilesLocations[pPiece->iNumTiles].iX = iTilePosX;
+    pPiece->aTilesLocations[pPiece->iNumTiles].iY = iTilePosY;
+    pPiece->iNumTiles++;
+}
+
+void piece_def_set_hole(PieceDef* pPiece, int iHolePosX, int iHolePosY)
+{
+    if( pPiece->bHasHole == true )
+    {
+        error("hole position has already been set");
+    }
+    pPiece->sHolePos.iX = iHolePosX;
+    pPiece->sHolePos.iY = iHolePosY;
+    pPiece->bHasHole = true;
+}
 
 
 /*
@@ -69,8 +177,28 @@ Material * material_create(int iNumRows, int iNumCols)
     return pMaterial;
 }
 
+void material_add_piece_def(Material* pMaterial, PieceDef* pPieceDef)
+{
+    // type de pMaterial: Material*
+    // type de *pMaterial: Material
+    // type de (*pMaterial).sPieceDefs: Array
+    // type de pMaterial->sPieceDefs: Array
+    // type de &(pMaterial->sPieceDefs): Array*
+    // & should be read as 'address of'
+    // * should be read as 'pointed by'
+    // eg : 'Material* p' should be read as 'Material pointed by p'
+    array_add(&(pMaterial->sPieceDefs), pPieceDef);
+}
+
 void material_delete(Material* pMaterial)
 {
+    // delete the piece definitions
+    for( int iPieceIndex = 0 ; iPieceIndex < pMaterial->sPieceDefs.iNumElements; iPieceIndex++ )
+    {
+        PieceDef* pPiece = pMaterial->sPieceDefs.ppElements[iPieceIndex];
+        piece_def_delete(pPiece);
+        pMaterial->sPieceDefs.ppElements[iPieceIndex] = NULL;
+    }
     // type of pMaterial : Material*
     // type of *pMaterial : Material
     // type of (*pMaterial).sPieceDefs : Array
@@ -84,6 +212,13 @@ void material_delete(Material* pMaterial)
 void material_print(Material* pMaterial)
 {
     printf("board size : %d x %d\n", pMaterial->iNumRows, pMaterial->iNumCols);
+
+    // print the piece definitions
+    for( int iPieceIndex = 0 ; iPieceIndex < pMaterial->sPieceDefs.iNumElements; iPieceIndex++ )
+    {
+        PieceDef* pPiece = pMaterial->sPieceDefs.ppElements[iPieceIndex];
+        piece_def_print(pPiece);
+    }
 }
 
 
@@ -108,18 +243,6 @@ typedef struct
     Direction eMoveDir;  // the direction in which the piece is moved
     bool bNutHasFallen;
 } Move;
-
-// maximum number of tiles covered by any piece
-#define MAX_PIECE_SIZE 3
-
-typedef struct 
-{
-    PieceId iPieceId;
-    int iNumTiles;
-    Position aTilesLocations[MAX_PIECE_SIZE];
-    bool bHasHole;
-    Position sHolePos;
-} PieceDef;
 
 typedef enum
 {
@@ -232,8 +355,41 @@ void find_solutions(Board* pBoard, PieceDef* pPieceDefs, PiecePos* pPiecesPos, i
 int main(int argc, char* argv[])
 {
     Material* pMaterial = material_create(4, 4);
+
+    PieceDef* p1 = piece_def_create(1);
+    piece_def_add_tile(p1, 0, 0);
+    piece_def_add_tile(p1, 1, 0);
+    piece_def_add_tile(p1, 0, 1);
+    piece_def_set_hole(p1, 0, 0);
+
+    PieceDef* p2 = piece_def_create(2);
+    piece_def_add_tile(p2, 0, 0);
+    piece_def_add_tile(p2, 1, 0);
+    piece_def_add_tile(p2, 0, 1);
+    piece_def_set_hole(p2, 1, 0);
+
+    PieceDef* p3 = piece_def_create(3);
+    piece_def_add_tile(p3, 0, 0);
+    piece_def_add_tile(p3, 1, 0);
+    piece_def_set_hole(p3, 1, 0);
+
+    PieceDef* p4 = piece_def_create(4);
+    piece_def_add_tile(p4, 0, 0);
+    piece_def_add_tile(p4, 1, 0);
+    piece_def_set_hole(p4, 1, 0);
+
+    PieceDef* p5 = piece_def_create(5);
+    piece_def_add_tile(p5, 0, 0);
+
+    material_add_piece_def(pMaterial, p1);
+    material_add_piece_def(pMaterial, p2);
+    material_add_piece_def(pMaterial, p3);
+    material_add_piece_def(pMaterial, p4);
+    material_add_piece_def(pMaterial, p5);
+
     material_print(pMaterial);
     material_delete(pMaterial);
+
     return 0;
 }
 
